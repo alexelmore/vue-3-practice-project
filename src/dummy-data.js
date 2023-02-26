8137d4d3f324277be164d7083de6b4732fac7aa9
aelmore49 <alexelmore49@gmail.com>
2023-02-26 09:15:21 -0600

    Initial project setup;taking old Vue 2 project I build years ago and convert it over to use Vue 3

---
+++ src/dummy-data.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 'alex',
+    fullName: 'Alex Robert Elmore',
+    projects: [
+      { id: 'alex_p1', title: 'Complete the Vue course' },
+      { id: 'alex_p2', title: 'Complete more courses.' },
+      { id: 'alex_p3', title: 'Keep learning.' }
+    ]
+  },
+  {
+    id: 'sara',
+    fullName: 'Sara Emami',
+    projects: [
+      { id: 'sara_p1', title: 'Wedding Planning.' },
+      { id: 'sara_p2', title: 'Play with Sherman.' },
+      { id: 'sara_p3', title: 'Hug Hanky.' }
+    ]
+  },
+  {
+    id: 'sherman',
+    fullName: 'Sherman Denver Dupree Elmore',
+    projects: [
+      { id: 'sherman_p1', title: 'Play with toys.' },
+      { id: 'sherman_p2', title: 'Bug Hanky.' }
+    ]
+  },
+  {
+    id: 'hank',
+    fullName: 'Henry Auto Emami',
+    projects: [{ id: 'hank_p1', title: 'Patrol for squirrels.' },
+    { id: 'hank_p2', title: 'Sleep on sofa.' }]
+  }
+];
